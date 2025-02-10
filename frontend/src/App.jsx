22fe802a592980b8427c4e28ba54db18ffddaa34
zakariae <sebaizakariae@gmail.com>
2025-02-10 15:10:25 +0100

    fix and update setting export name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,17 @@ import {Routes, Route, Navigate} from "react-router-dom"
 import Navbar from "./components/Navbar.jsx"
 import HomePage from "./pages/HomePage.jsx"
 import ProfilePage from "./pages/ProfilePage.jsx"
-import SettingPage from "./pages/SettingPage.jsx"
+import SettingsPage from "./pages/SettingPage.jsx"
 import SignupPage from "./pages/SignupPage.jsx"
 import SigninPage from "./pages/SigninPage.jsx"
 import { useAuthStore } from "./store/useAuthStore.js"
 import { useEffect } from "react"
 import {Loader} from "lucide-react"
 import { Toaster } from "react-hot-toast"
+import { useThemeStore } from "./store/useThemeStore.js"
 function App() {
   const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
-
+  const {theme} = useThemeStore();
   useEffect(() => {
     checkAuth();
   },[checkAuth])
@@ -27,14 +28,14 @@ function App() {
   }
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar/>
       <Routes>
         <Route path="/" element={authUser ? <HomePage/>:<Navigate to="/login"/>}/>
         <Route path="/signup" element={!authUser? <SignupPage/> : <Navigate to="/"/>}/>
         <Route path="/login" element={!authUser? <SigninPage/> : <Navigate to="/"/>}/>
         <Route path="/profile" element={authUser? <ProfilePage/>: <Navigate to="/login"/>}/>
-        <Route path="/settings" element={authUser? <SettingPage/>: <Navigate to="/login"/>}/>
+        <Route path="/settings" element={authUser? <SettingsPage/>: <Navigate to="/login"/>}/>
       </Routes>
       <Toaster/>
     </div>
