8105574fac3d61b2ef327ab554470a94a275da29
zakariae <sebaizakariae@gmail.com>
2025-02-20 16:28:15 +0100

    init socket.io in backend

---
+++ backend/src/lib/socket.js
@@ -0,0 +1,18 @@
+import {Server} from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {cors: {origin: "http://localhost:5173"}});
+
+io.on("connection", (socket) => {
+    console.log("a user connected", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("a user disconnected", socket.id);
+    });
+});
+
+
+export {io, app, server};
--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,9 @@ import connectDB from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import messagesRoutes from "./routes/message.route.js";
 import cors from "cors";
+import {app, server} from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
 const port = process.env.PORT || 5001;
 app.use(express.json());
 app.use(express.json({ limit: "10mb" }));
@@ -22,7 +22,7 @@ app.use("/api/messages", messagesRoutes);
 
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
     connectDB();
 })
